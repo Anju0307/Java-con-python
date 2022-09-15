d9da6d371470f6f8a323e5c327fbbafa17fced73
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-15 17:46:38 -0500

    Ejercicio 17

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-let parcial1 = parseInt(prompt('Ingrese la nota del parcial 1'));
-let parcial2 = parseInt(prompt('Ingrese la nota del parcial 2'));
-let parcial3 = parseInt(prompt('Ingrese la nota del parcial 3'));
-let examenF = parseInt(prompt('Ingrese la nota del examen final'));
-let trabajoF = parseInt(prompt('Ingrese la nota del trabajo final'));
+let iva = parseInt(prompt('Ingrese el iva del producto sin %'));
+let producto = parseInt(prompt('Ingrese el precio del producto'));
 
-let nota = (((parcial1+parcial2+parcial3)/3)*0.55)+(examenF*0.3)+(trabajoF*0.15);
+let ivaP=producto*(iva/100);
+let total=producto+ivaP;
 
-document.getElementById('nota').innerHTML= nota;
+document.getElementById('pagar').innerHTML=total;
+document.getElementById('iva').innerHTML=ivaP;
