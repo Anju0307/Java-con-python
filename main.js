3fc1e35beba93a02e3667ac8e9ae02fac09ba66d
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-15 17:40:56 -0500

    Ejercicio 16

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-let grados = parseInt(prompt('Ingrese los grados en celcius'));
+let parcial1 = parseInt(prompt('Ingrese la nota del parcial 1'));
+let parcial2 = parseInt(prompt('Ingrese la nota del parcial 2'));
+let parcial3 = parseInt(prompt('Ingrese la nota del parcial 3'));
+let examenF = parseInt(prompt('Ingrese la nota del examen final'));
+let trabajoF = parseInt(prompt('Ingrese la nota del trabajo final'));
 
-let conversion = (grados*9/5)+32;
+let nota = (((parcial1+parcial2+parcial3)/3)*0.55)+(examenF*0.3)+(trabajoF*0.15);
 
-document.getElementById('conversion').innerHTML=conversion;
+document.getElementById('nota').innerHTML= nota;
