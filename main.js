bbf2af021f3faf8832fc27f75241ee99a75d3db6
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-15 16:24:37 -0500

    Ejercicio 11

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const x = 3;
-
 let a = parseInt(prompt('Ingrese el valor de a'));
 let b = parseInt(prompt('Ingrese el valor de b'));
 let c = parseInt(prompt('Ingrese el valor de c'));
 
-let raiz = a * x**2 + b * x + c;
+let volumen = (4/3) * Math.PI * a * b * c;
 
-document.getElementById('raiz').innerHTML=raiz;
+document.getElementById('volumen').innerHTML=volumen;
