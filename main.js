34451ea928752caf46d7837202fd432899d1ef0f
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-15 17:30:04 -0500

    Ejercicio 14

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-const precioMin=355;
+let cantidadMed=parseInt(prompt('Ingrese la cantidad de medicamentos'));
+let totalMed=0;
+for (let i=0;i<cantidadMed;i++){
+    let precioMed=parseInt(prompt(`Ingrese el precio del producto ${i+1}`));
+    totalMed=precioMed+totalMed;
+}
 
-let minuto = parseInt(prompt('Ingrese la cantidad de minutos hablados'));
-
-let pagar = minuto*precioMin+((minuto*precioMin)*0.2);
-
-document.getElementById('pagar').innerHTML=pagar;
+let descuento = totalMed*0.10;
+let precioFin = totalMed-descuento;
+console.log(descuento)
+document.getElementById('descuento').innerHTML= descuento;
+document.getElementById('precio').innerHTML=precioFin;
