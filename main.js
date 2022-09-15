525ef644c5ab87c45a04332a00fe636961186513
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-15 17:33:23 -0500

    Ejercicio 15

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-let cantidadMed=parseInt(prompt('Ingrese la cantidad de medicamentos'));
-let totalMed=0;
-for (let i=0;i<cantidadMed;i++){
-    let precioMed=parseInt(prompt(`Ingrese el precio del producto ${i+1}`));
-    totalMed=precioMed+totalMed;
-}
+let grados = parseInt(prompt('Ingrese los grados en celcius'));
 
-let descuento = totalMed*0.10;
-let precioFin = totalMed-descuento;
-console.log(descuento)
-document.getElementById('descuento').innerHTML= descuento;
-document.getElementById('precio').innerHTML=precioFin;
+let conversion = (grados*9/5)+32;
+
+document.getElementById('conversion').innerHTML=conversion;
